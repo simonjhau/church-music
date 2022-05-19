26be15779b3cad4321378430e702ee8243f906a7
Simon Au <simon@aumail.org>
2022-05-19 23:32:05 +0100

    Remove api code and move app into root folder

--- api/src/models/fileTypes.ts
+++
@@ -1,7 +0,0 @@
-import { dbQuery } from './db';
-
-export const dbGetFiletypes = async () => {
-  let sqlQuery = `SELECT * FROM file_types ORDER BY id;`;
-  const books = await dbQuery(sqlQuery);
-  return books.rows;
-};
--- api/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import { readdir, unlink } from 'fs/promises';
-import path from 'path';
-
-export const deleteFilesInDirectory = async (directory: string) => {
-  const files = await readdir(directory);
-  for (const file of files) {
-    unlink(path.join(directory, file));
-  }
-};
