f4dd0f8699a1a1a2fe1edbb41f0c17d62d6b7f58
Simon Au <simon@aumail.org>
2023-02-04 14:12:10 +0000

    Make typescript extra strict

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,16 +8,16 @@ const port = process.env.PORT ?? 9000;
 const appDir = path.join(__dirname, "../..", "app", "dist");
 app.use(express.static(appDir));
 
-app.get("/api/crash", (req, res) => {
+app.get("/api/crash", (_req, _res) => {
   throw new Error("crash");
 });
 
-app.get("/api", (req, res) => {
+app.get("/api", (_req, res) => {
   console.log("api");
   res.send("Hello World from api!!! Let's make a change");
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   console.log("get html");
   res.sendFile(path.join(appDir, "index.html"));
 });
